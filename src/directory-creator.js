9f89f13ebe098a2f3772db7b2dd4befd96b2fb60
Max C <smashmouthmax@gmail.com>
2022-05-05 13:12:43 -0700

    added delete functionality with icons appearing on selected folder

--- src/directory-creator.js
+++ src/directory-creator.js
@@ -3,8 +3,8 @@ import { createHTMLElement } from "./index";
 const LOCAL_STORAGE_DIR_KEY = 'task.list';
 const LOCAL_STORAGE_FOLDER_KEY = 'task.selectedFolder';
 
+//local storage keys + array
 let selectedFolder = localStorage.getItem(LOCAL_STORAGE_FOLDER_KEY)
-
 let directoryArr = JSON.parse(localStorage.getItem(LOCAL_STORAGE_DIR_KEY)) || []; 
 
 const saveAndRenderList = () => {
@@ -12,10 +12,13 @@ const saveAndRenderList = () => {
     renderLists();
 }
 
+//saves to local storage
 const save = () => {
     localStorage.setItem(LOCAL_STORAGE_DIR_KEY, JSON.stringify(directoryArr));
+    localStorage.setItem(LOCAL_STORAGE_FOLDER_KEY, selectedFolder);
 }
 
+//renders the directory list
 const renderLists = () => {
     const directoryContainer = document.querySelector('.directory');
 
@@ -25,26 +28,35 @@ const renderLists = () => {
        directoryFolder.dataset.listId = directory.id;
        if(directory.id === selectedFolder) {
            directoryFolder.classList.add('directory-active');
-           console.log('this works');
        };
        directoryContainer.appendChild(directoryFolder); 
+
+       const directoryFolderIcons = createHTMLElement('span', null, ['directory-folder-icons'], null);
+       directoryFolderIcons.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-edit" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" /><line x1="13.5" y1="6.5" x2="17.5" y2="10.5" /></svg><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-trash" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
+       if(directoryFolder.classList.contains('directory-active')) {
+       directoryFolder.appendChild(directoryFolderIcons);
+       };
     })
 };
 
+//clears the directory list to be re-rendered in renderLists
 const clearList = (list) => {
     while(list.firstChild) {
         list.removeChild(list.firstChild)
     }
 };
 
+//adds eventlisteners for the directory
 const createDirListener = () => {
     const dirForm = document.querySelector('#add-dir-form');
     const dirContainer = document.querySelector('.directory');
 
     dirForm.addEventListener('submit', createDirectory)
-    dirContainer.addEventListener('click', selectedDirectory)
+    dirContainer.addEventListener('click', selectedDirectoryListener)
+    dirContainer.addEventListener('click', deleteDir)
 }
 
+//creates a new directory folder
 const createDirectory = (e) => {
     const dirText = document.querySelector('.add-dir-btn-text');
 
@@ -57,13 +69,23 @@ const createDirectory = (e) => {
     saveAndRenderList();
 }
 
-const selectedDirectory = (e) => {
+//function for the selected directory listener
+const selectedDirectoryListener = (e) => {
     if(e.target.tagName.toLowerCase() === 'li') {
         selectedFolder = e.target.dataset.listId;
         saveAndRenderList();
     }
 }
 
+const deleteDir = (e) => {
+    if(e.target.classList.contains('icon-trash') && e.target.parentElement.parentElement.dataset.listId === selectedFolder) {
+        directoryArr = directoryArr.filter(directory => directory.id !== selectedFolder);
+        selectedFolder = null;
+        saveAndRenderList();
+    }
+}
+
+//creates a directory folder object to be pushed into directory array
 const createNewDir = (dir) => {
    return { id: Date.now().toString(), name: dir, tasks: [] }
 }
