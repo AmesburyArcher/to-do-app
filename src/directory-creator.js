d5eb8433c728898493a16ac0713a740143577cb3
Max C <smashmouthmax@gmail.com>
2022-05-06 11:44:59 -0700

    added modal styling for edit folder name

--- src/directory-creator.js
+++ src/directory-creator.js
@@ -33,7 +33,7 @@ const renderLists = () => {
        directoryContainer.appendChild(directoryFolder); 
 
        const directoryFolderIcons = createHTMLElement('span', null, ['directory-folder-icons'], null);
-       directoryFolderIcons.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-edit" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" /><line x1="13.5" y1="6.5" x2="17.5" y2="10.5" /></svg><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-trash" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
+       directoryFolderIcons.innerHTML = '<button class="button-edit"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-edit" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" /><line x1="13.5" y1="6.5" x2="17.5" y2="10.5" /></svg></button><button class="button-trash"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-trash" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg></button>';
        //if the folder is selected, add the icons
        if(directoryFolder.classList.contains('directory-active')) {
        directoryFolder.appendChild(directoryFolderIcons);
@@ -82,7 +82,7 @@ const selectedDirectoryListener = (e) => {
 
 //function for the deletedir listener 
 const deleteDir = (e) => {
-    if(e.target.classList.contains('icon-trash') && e.target.parentElement.parentElement.dataset.listId === selectedFolder) {
+    if(e.target.classList.contains('icon-trash') && e.target.parentElement.parentElement.parentElement.dataset.listId === selectedFolder) {
         directoryArr = directoryArr.filter(directory => directory.id !== selectedFolder);
         selectedFolder = null;
         saveAndRenderList();
@@ -90,9 +90,9 @@ const deleteDir = (e) => {
 }
 //Editing the directory folder title and updating the array + DOM
 const editDir = (e) => {
-    if(e.target.classList.contains('icon-edit') && e.target.parentElement.parentElement.dataset.listId === selectedFolder) {
+    if(e.target.classList.contains('icon-edit') && e.target.parentElement.parentElement.parentElement.dataset.listId === selectedFolder) {
         const directoryModal = document.querySelector('.directory-modal-container');
-        directoryModal.style.display = 'block';
+        directoryModal.classList.toggle('active');
         const editDirBtn = document.querySelector('#directory-edit-form');
 
         const editDirForm = (e) => {
@@ -108,12 +108,13 @@ const editDir = (e) => {
             directoryModalText.value = null;
             saveAndRenderList();
             const directoryModal = document.querySelector('.directory-modal-container');
-            directoryModal.style.display = 'none';
+            directoryModal.classList.toggle('active');
             editDirBtn.removeEventListener('submit', editDirForm);
         }
 
         editDirBtn.addEventListener('submit', editDirForm)
     }
+    
 
 }
 
