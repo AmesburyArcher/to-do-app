f0797714d4baeaaae5a96ae4cd6d6697c26d4d9f
Max C <smashmouthmax@gmail.com>
2022-05-06 08:42:36 -0700

    added edit directory functionality

--- src/directory-creator.js
+++ src/directory-creator.js
@@ -26,6 +26,7 @@ const renderLists = () => {
     directoryArr.forEach(directory => {
        const directoryFolder = createHTMLElement('li', null, ['directory-folder'], directory.name)
        directoryFolder.dataset.listId = directory.id;
+       //if the folder is selected, make it active folder
        if(directory.id === selectedFolder) {
            directoryFolder.classList.add('directory-active');
        };
@@ -33,6 +34,7 @@ const renderLists = () => {
 
        const directoryFolderIcons = createHTMLElement('span', null, ['directory-folder-icons'], null);
        directoryFolderIcons.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-edit" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4" /><line x1="13.5" y1="6.5" x2="17.5" y2="10.5" /></svg><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-trash" width="18" height="18" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>';
+       //if the folder is selected, add the icons
        if(directoryFolder.classList.contains('directory-active')) {
        directoryFolder.appendChild(directoryFolderIcons);
        };
@@ -54,6 +56,7 @@ const createDirListener = () => {
     dirForm.addEventListener('submit', createDirectory)
     dirContainer.addEventListener('click', selectedDirectoryListener)
     dirContainer.addEventListener('click', deleteDir)
+    dirContainer.addEventListener('click', editDir)
 }
 
 //creates a new directory folder
@@ -77,12 +80,42 @@ const selectedDirectoryListener = (e) => {
     }
 }
 
+//function for the deletedir listener 
 const deleteDir = (e) => {
     if(e.target.classList.contains('icon-trash') && e.target.parentElement.parentElement.dataset.listId === selectedFolder) {
         directoryArr = directoryArr.filter(directory => directory.id !== selectedFolder);
         selectedFolder = null;
         saveAndRenderList();
     }
+}
+//THIS NEEDS TO BE WORKED ON********
+const editDir = (e) => {
+    if(e.target.classList.contains('icon-edit') && e.target.parentElement.parentElement.dataset.listId === selectedFolder) {
+        const directoryModal = document.querySelector('.directory-modal-container');
+        directoryModal.style.display = 'block';
+        let directoryID = e.target.parentElement.parentElement.dataset.listId;
+        let i = directoryArr.map(a => a.id).indexOf(directoryID);
+        const editDirBtn = document.querySelector('#directory-edit-form');
+
+        const editDirForm = (e) => {
+            e.preventDefault();
+            const directoryModalText = document.querySelector('#text-dir');
+            const directoryReName = directoryModalText.value;
+            if(directoryReName == null || directoryReName === '') return;
+            const newDirectoryFolder = createNewDir(directoryReName);
+            directoryModalText.value = null;
+    
+            directoryArr.splice(i, 1, newDirectoryFolder);
+            saveAndRenderList();
+            console.log(i);
+            const directoryModal = document.querySelector('.directory-modal-container');
+            directoryModal.style.display = 'none';
+            editDirBtn.removeEventListener('submit', editDirForm);
+        }
+
+        editDirBtn.addEventListener('submit', editDirForm)
+    }
+
 }
 
 //creates a directory folder object to be pushed into directory array
