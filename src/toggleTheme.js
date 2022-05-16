cfb680b9bdea215c2c9f96a18c2666096187fde5
Max C <smashmouthmax@gmail.com>
2022-05-15 17:54:16 -0700

    added local storage to toggleTheme

--- src/toggleTheme.js
+++ src/toggleTheme.js
@@ -3,11 +3,27 @@ const toggleTheme = () => {
 const toggle = document.querySelector('.light-dark-mode');
 toggle.addEventListener('click', toggleMode);
 
-function toggleMode() {
-    document.body.classList.toggle('active');
-    toggle.classList.toggle('active');
+let darkMode = localStorage.getItem('darkMode');
+const currentTheme = document.querySelector('.current-theme');
 
-    const currentTheme = document.querySelector('.current-theme');
+if(darkMode === 'enabled') {
+    localStorage.setItem('darkMode', 'enabled')
+    document.body.classList.add('active');
+    toggle.classList.add('active');
+    currentTheme.textContent = 'Dark Mode'
+}
+
+function toggleMode() {
+    darkMode = localStorage.getItem('darkMode')
+    if(darkMode !== 'enabled') {
+        localStorage.setItem('darkMode', 'enabled')
+        document.body.classList.add('active');
+        toggle.classList.add('active');
+    } else {
+        localStorage.setItem('darkMode', null)
+        document.body.classList.remove('active');
+        toggle.classList.remove('active');
+    }
 
     if(toggle.classList.contains('active')) { 
         currentTheme.textContent = 'Dark Mode';
